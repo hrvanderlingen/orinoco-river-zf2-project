4437d4773c579a050ed4b078c7ed71aaeba26481
Rik <info@assessmentee.com>
2014-07-20 16:02:31 +0200

    register zend class map

--- init_autoloader.php
+++ init_autoloader.php
@@ -3,4 +3,12 @@
 if (file_exists('vendor/autoload.php')) {
     $loader = include 'vendor/autoload.php';
 }
+// generate this class map
+// ./path/to/bin/classmap_generator.php -w --library path/to/Zend --output path/to/Zend/Zendautoload_classmap.php
+$classMap = require __DIR__ . '/vendor/zendframework/zendframework/library/Zend/zend_autoload_classmap.php';
 
+// Register the class map:
+$loader->addClassMap($classMap);
+
+// Register with spl_autoload:
+$loader->register(true);
