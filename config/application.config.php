d874e92581f3e98b73e7754ed70103b691f497ec
Rik <info@assessmentee.com>
2015-01-25 16:08:36 +0100

    move config section to module_listener_options array

--- config/application.config.php
+++ config/application.config.php
@@ -11,26 +11,26 @@ return array(
         'module_paths' => array(
             './module',
             './vendor'
-        )
+        ),
+        // Whether or not to enable a module class map cache.
+        // If enabled, creates a module class map cache which will be used
+        // by in future requests, to reduce the autoloading process.
+        'module_map_cache_enabled' => ($env == 'production'),
+        // The key used to create the class map cache file name.
+        'module_map_cache_key' => "H32B3HEU3SUWY3",
+        // The path in which to cache merged configuration.
+        'cache_dir' => __dir__ . '/../data/cache',
+        // Whether or not to enable modules dependency checking.
+        // Enabled by default, prevents usage of modules that depend on other modules
+        // that weren't loaded.
+        'check_dependencies' => ($env == 'development'),
+        // set the autoload path
+        'config_glob_paths' => array(
+            'config/autoload/{,*.}{global,local}.php',
+        ),
+        // set config caching
+        'config_cache_enabled' => false,
+        // The key used to create the configuration cache file name.
+        'config_cache_key' => 'FHT2837EHDRT4653G',
     ),
-    // Whether or not to enable a module class map cache.
-    // If enabled, creates a module class map cache which will be used
-    // by in future requests, to reduce the autoloading process.
-    'module_map_cache_enabled' => ($env == 'production'),
-    // The key used to create the class map cache file name.
-    'module_map_cache_key' => "H32B3HEU3SUWY3",
-    // The path in which to cache merged configuration.
-    'cache_dir' => __dir__ . '/../data/cache',
-    // Whether or not to enable modules dependency checking.
-    // Enabled by default, prevents usage of modules that depend on other modules
-    // that weren't loaded.
-    'check_dependencies' => ($env == 'development'),
-    // set the autoload path
-    'config_glob_paths' => array(
-        'config/autoload/{,*.}{global,local}.php',
-    ),
-    // set config caching
-    'config_cache_enabled' => ($env == 'production'),
-    // The key used to create the configuration cache file name.
-    'config_cache_key' => 'FHT2837EHDRT4653G',
 );
