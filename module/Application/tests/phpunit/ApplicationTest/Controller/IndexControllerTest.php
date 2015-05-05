87a8228c358e8a1d017f1fdf7bcb3f840f457c2a
Rik <info@assessmentee.com>
2015-05-05 22:47:42 +0200

    unit test for valid html on home page

--- module/Application/tests/phpunit/ApplicationTest/Controller/IndexControllerTest.php
+++ module/Application/tests/phpunit/ApplicationTest/Controller/IndexControllerTest.php
@@ -6,24 +6,31 @@ use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
 
 class IndexControllerTest extends AbstractHttpControllerTestCase
 {
+
     public function setUp()
     {
         $this->setApplicationConfig(
-            include __DIR__ . '/../../../../../../config/application.config.php'
+                include __DIR__ . '/../../../../../../config/application.config.php'
         );
         parent::setUp();
     }
-    
-    public function testIndexActionCanBeAccessed()
-{
-    $this->dispatch('/');
-    $this->assertResponseStatusCode(200);
 
-    $this->assertModuleName('Application');
-    $this->assertControllerName('Application\Controller\Index');
-    $this->assertControllerClass('IndexController');
-    $this->assertMatchedRouteName('home');
-}
-    
-    
+    public function testIndexActionCanBeAccessed()
+    {
+        $this->dispatch('/');
+        $this->assertResponseStatusCode(200);
+        $this->assertModuleName('Application');
+        $this->assertControllerName('Application\Controller\Index');
+        $this->assertControllerClass('IndexController');
+        $this->assertMatchedRouteName('home');
+    }
+
+    public function testIndexActionHasValidHtml()
+    {
+        $this->dispatch('/');
+        $this->assertQuery('html');
+        $this->assertQuery('head');
+        $this->assertQuery('body');
+    }
+
 }
