0975bec2bf2aa9597f3d06f05d0247715e6eef83
Rik <info@assessmentee.com>
2015-07-05 16:37:50 +0200

    retire Bower

--- init_autoloader.php
+++ init_autoloader.php
@@ -6,10 +6,18 @@ if (file_exists('vendor/autoload.php')) {
 // generate Zend Framework class map
 // move to the vendor folder and execute
 // ./bin/classmap_generator.php -w --library zendframework/zendframework/library/Zend --output zend_autoload_classmap.php
-$classMap = require __DIR__ . '/vendor/zend_autoload_classmap.php';
 
-// Register the class map:
-$loader->addClassMap($classMap);
+$classMapLocation = __DIR__ . '/vendor/zend_autoload_classmap.php';
 
-// Register with spl_autoload:
-$loader->register(true);
+if(file_exists($classMapLocation)){
+    $classMap = require __DIR__ . '/vendor/zend_autoload_classmap.php';
+    // Register the class map:
+    $loader->addClassMap($classMap);
+
+    // Register with spl_autoload:
+    $loader->register(true);
+}
+
+
+
+
