9d77438c37a2164ccf14544e543f686bd60b70d9
Rik <info@assessmentee.com>
2017-07-15 12:00:00 +0200

    tree page configuration

--- config/autoload/chemistry.global.php
+++ config/autoload/chemistry.global.php
@@ -4,15 +4,17 @@ $settings = array();
 
 return array(
     'chemistry' => $settings,
+    'treeSupplier' => 'wikipedia',
     'service_manager' => array(
 	'aliases' => array(),
     ),
     'bjyauthorize' => array(
 	'guards' => array(
 	    'BjyAuthorize\Guard\Controller' => array(
-		array('controller' => 'chemistry', 'roles' => array('guest', 'user', 'admin')),
+		array('controller' => 'Chemical\Controller\IndexController', 'roles' => array('guest', 'user', 'admin')),
                 array('controller' => 'rest', 'roles' => array('guest', 'user', 'admin')),
-	    ),
+                array('controller' => 'Tree', 'roles' => array('guest', 'user', 'admin')),
+            ),
 	),
     ),
 );
