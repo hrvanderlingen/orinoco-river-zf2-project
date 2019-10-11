e3410f5f25507a335cc59d8e5841e7d49d326ca7
rik <hrvanderlingen@gmail.com>
2019-10-11 18:27:58 +0200

    blank default view

--- config/autoload/chemical.global.php
+++ config/autoload/chemical.global.php
@@ -4,4 +4,5 @@ return [
     'treeStore' => "path/to/folder/for/storing/tree/xml/files",
     'jwt_secret' => 'your-secret-here',
     'jwt_servername' => 'your-server-name-here',
+    'Access-Control-Allow-Origin' => 'your-host-name-here'
 ];
