2713ce6e420f19f0241aeb80c4457b5bcc3a580a
rik <hrvanderlingen@gmail.com>
2019-11-02 18:34:45 +0100

    composer update

--- config/autoload/chemical.global.php
+++ config/autoload/chemical.global.php
@@ -4,5 +4,6 @@ return [
     'treeStore' => "path/to/folder/for/storing/tree/xml/files",
     'jwt_secret' => 'your-secret-here',
     'jwt_servername' => 'your-server-name-here',
-    'Access-Control-Allow-Origin' => 'your-host-name-here'
+    'Access-Control-Allow-Origin' => 'your-host-name-here',
+    'rsc_key' => 'your-key-here'
 ];
