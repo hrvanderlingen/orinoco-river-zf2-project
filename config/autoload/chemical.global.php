f934b0059fcfb5dfd8e1d20377dede3ec16b3703
rik <hrvanderlingen@gmail.com>
2019-11-22 14:05:35 +0100

    main layout update

--- config/autoload/chemical.global.php
+++ config/autoload/chemical.global.php
@@ -5,5 +5,9 @@ return [
     'jwt_secret' => 'your-secret-here',
     'jwt_servername' => 'your-server-name-here',
     'Access-Control-Allow-Origin' => 'your-host-name-here',
-    'rsc_key' => 'your-key-here'
+    'rsc_key' => 'your-key-here',
+    'existdb_Host' => 'http://localhost:8082',
+    'existdb_Path' => '/exist/rest',
+    'existdb_User' => 'admin',
+    'existdb_Pass' => ''
 ];
